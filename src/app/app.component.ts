e2466ef0953a74378a25fe04cca6c8551446172b
i3oc9i <ivano.coltellacci@gmail.com>
2024-05-22 17:01:14 +0200

    Add playing card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PlayingCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'learning_angular';
+
 }
