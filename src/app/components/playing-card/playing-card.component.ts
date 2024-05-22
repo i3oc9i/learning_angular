f6bfb7aecde38174f255366df4fba1d5cb845f1e
i3oc9i <ivano.coltellacci@gmail.com>
2024-05-22 17:11:38 +0200

    Update playing card component layout and content

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './playing-card.component.html',
   styleUrl: './playing-card.component.css'
 })
-export class PlayingCardComponent {
 
+export class PlayingCardComponent {
+  name: string = "My Monster";
+	hp: number = 40;
+	figureCaption: string = "N°001 Monster"
+	attackName: string = "Geo Impact"
+	attackStrength: number = 60;
+	attackDescription: string = "This is a long description of a monster capacity.";
 }
