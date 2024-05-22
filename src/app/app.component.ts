cd3a570d2cf90a124abec5e30b73a45a3a34c065
i3oc9i <ivano.coltellacci@gmail.com>
2024-05-22 17:55:09 +0200

    chore: Update playing card component with new monster data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
+import { Monster } from './models/monster-model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,19 @@ import { PlayingCardComponent } from './components/playing-card/playing-card.com
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  monster1!: Monster;
+  monster2!: Monster;
+
+  constructor() {
+    this.monster1 = new Monster();
+    this.monster1.name = 'Pik';
+    this.monster1.hp = 40;
+    this.monster1.figureCaption = 'N°002 Pik';
+
+    this.monster2 = new Monster();
+    this.monster2.name = 'Puk';
+    this.monster2.hp = 45;
+    this.monster2.figureCaption = 'N°002 Puk';
+  }
+}
--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Monster } from '../../models//monster-model';
 
 @Component({
   selector: 'app-playing-card',
@@ -9,11 +10,13 @@ import { Component, Input } from '@angular/core';
   styleUrl: './playing-card.component.css',
 })
 export class PlayingCardComponent {
-  @Input() name: string = 'My Monster';
-  @Input() hp: number = 40;
-  @Input() figureCaption: string = 'N°001 Monster';
-  @Input() attackName: string = 'Geo Impact';
-  @Input() attackStrength: number = 60;
-  @Input() attackDescription: string =
-    'This is a long description of a monster capacity.';
+  @Input({
+    required: true,
+    alias: 'my.monster',
+    transform: (value: Monster) => {
+      value.hp = Math.round(value.hp / 2);
+      return value;
+    },
+  })
+  monster: Monster = new Monster();
 }
