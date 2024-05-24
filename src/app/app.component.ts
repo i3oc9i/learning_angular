bf654e0f51b89f24cbadc0de33286747b16c1d92
i3oc9i <ivano.coltellacci@gmail.com>
2024-05-25 01:31:08 +0200

    chore: Update app component with search bar and click counter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal, computed, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, computed, effect, model } from '@angular/core';
 
 import { Monster } from './models/monster-model';
 import { MonsterType } from './utils/monster-utils';
@@ -9,12 +10,19 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayingCardComponent, SearchBarComponent],
+  imports: [CommonModule, PlayingCardComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   monsters!: Monster[];
+
+  search = model('');
+  filteredMonsters = computed(() => {
+    return this.monsters.filter((monster) =>
+      monster.name.includes(this.search())
+    );
+  });
 
   selectedMonsterIndex = signal(1);
   selectedMonster = computed(() => {
@@ -30,10 +38,10 @@ export class AppComponent {
 
     const monster1 = new Monster();
     monster1.type = MonsterType.FIRE;
-    monster1.name = 'Pik';
+    monster1.name = 'Hot';
     monster1.hp = 40;
     monster1.figure = './assets/img/fig_fire.png';
-    monster1.figureCaption = 'N°001 Pik';
+    monster1.figureCaption = 'N°001 Hot';
     monster1.attackName = 'Fire Attack';
     monster1.attackStrength = 25;
     monster1.attackDescription = 'This is a fire attack description...';
@@ -41,10 +49,10 @@ export class AppComponent {
 
     const monster2 = new Monster();
     monster2.type = MonsterType.PLANT;
-    monster2.name = 'Car';
+    monster2.name = 'Bulb';
     monster2.hp = 60;
     monster2.figure = './assets/img/fig_plant.png';
-    monster2.figureCaption = 'N°002 Car';
+    monster2.figureCaption = 'N°002 Bulb';
     monster2.attackName = 'Plant Attack';
     monster2.attackStrength = 35;
     monster2.attackDescription = 'This is a plant attack description...';
@@ -60,6 +68,17 @@ export class AppComponent {
     monster3.attackStrength = 15;
     monster3.attackDescription = 'This is a water attack description...';
     this.monsters.push(monster3);
+
+    const monster4 = new Monster();
+    monster4.type = MonsterType.ELECTRIC;
+    monster4.name = 'Flash';
+    monster4.hp = 40;
+    monster4.figure = './assets/img/fig_electric.png';
+    monster4.figureCaption = 'N°003 Flash';
+    monster4.attackName = 'Electric Attack';
+    monster4.attackStrength = 55;
+    monster4.attackDescription = 'This is a electric attack description...';
+    this.monsters.push(monster4);
   }
 
   toggleMonster() {
